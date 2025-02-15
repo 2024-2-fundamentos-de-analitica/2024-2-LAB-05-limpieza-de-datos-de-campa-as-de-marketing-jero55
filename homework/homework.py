c069d4709667511a2ff6fe3fd590812a7fd7b91d
Jeronimo S.S. <jsalazars@unal.edu.co>
2025-02-15 12:11:24 -0500

    Tarea completa

--- homework/homework.py
+++ homework/homework.py
@@ -4,6 +4,8 @@ Escriba el codigo que ejecute la accion solicitada.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
+import os
 
 def clean_campaign_data():
     """
@@ -50,8 +52,47 @@ def clean_campaign_data():
 
     """
 
+    archivos = os.listdir("files/input/")
+    columnas =["client_id","age","job","marital","education","credit_default","mortgage","month","day","contact_duration","number_contacts","previous_campaign_contacts","previous_outcome","cons_price_idx","euribor_three_months","campaign_outcome"]
+    df = pd.DataFrame(columns=columnas,index=None)
+    # limitador = 0
+    for archivo in archivos:
+        # if limitador == 1:
+        #     break
+        if not archivo.endswith(".zip"):
+            continue
+        df_archivo = pd.read_csv(f"files/input/{archivo}", compression="zip",index_col=0)
+        df = pd.concat([df, df_archivo], ignore_index=True)
+        # limitador += 1
+
+    df_cliente = df[["client_id","age","job","marital","education","credit_default","mortgage"]].copy()
+    df_cliente["job"] = df_cliente["job"].str.replace(".","").str.replace("-","_")
+    df_cliente["education"] = df_cliente["education"].str.replace(".","_").replace("unknown",pd.NA)
+    df_cliente["credit_default"] = df_cliente["credit_default"].apply(lambda x: 1 if x == "yes" else 0)
+    df_cliente["mortgage"] = df_cliente["mortgage"].apply(lambda x: 1 if x == "yes" else 0)
+    df_cliente.dropna(how="all")
+    # print(df_cliente.shape)
+
+    meses = {"jan": "01","feb":"02","mar":"03","apr":"04","may":"05","jun":"06","jul":"07","aug":"08","sep":"09","oct":"10","nov":"11","dec":"12"}
+
+    df_campaign = df[["client_id","number_contacts","contact_duration","previous_campaign_contacts","previous_outcome","campaign_outcome","month","day"]].copy()
+    df_campaign["previous_outcome"] = df_campaign["previous_outcome"].apply(lambda x: 1 if x == "success" else 0)
+    df_campaign["campaign_outcome"] = df_campaign["campaign_outcome"].apply(lambda x: 1 if x == "yes" else 0)
+    df_campaign["last_contact_date"] = "2022-" + df_campaign["month"].apply(lambda x: meses[x] ).astype(str) +"-"+ df_campaign["day"].astype(str)  
+    df_campaign.dropna(how="all")
+    df_campaign.drop(columns=["month","day"],inplace=True)
+    # print(df_campaign.shape)
+
+    df_economics = df[["client_id","cons_price_idx","euribor_three_months"]].copy()
+    df_economics.dropna(how="all")
+    # print(df_economics.shape)
+
+    df_cliente.to_csv("files/output/client.csv",index=False)
+    df_campaign.to_csv("files/output/campaign.csv",index=False)
+    df_economics.to_csv("files/output/economics.csv",index=False)
     return
 
 
 if __name__ == "__main__":
+    # print("----------------------------------------------------------------------------------")
     clean_campaign_data()
